64b266079b8fe0329238c4b1ef5f4218506df55c
Barbara <varya.sheptunova@gmail.com>
2023-03-26 16:12:04 +0300

    Count birds

---
+++ Task2/task2.py
@@ -0,0 +1,7 @@
+print("Введите количество журавликов: ")
+all_birds = int(input())
+birds_k = (all_birds // 3) * 2
+birds_s = (all_birds // 3) // 2
+birds_p = birds_s
+
+print(f"Катя: {birds_k}, Сережа: {birds_s}, Петя: {birds_p}")
